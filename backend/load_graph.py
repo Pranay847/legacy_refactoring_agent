4c325cdd12747dd9165998cef80fac4ee8b24ca7
Pranay Karakoti <pranay846@outlook.com>
2026-04-05 16:45:55 -0500

    Remove dead graph_loader.py, fix Neo4j default password, add site-packages to skip list
    - Delete backend/graph_analysis/graph_loader.py (dead code, nothing imports it)
    - Fix Neo4j password default in load_graph.py to match .env (surgeon1234)
    - Add site-packages, .eggs, egg-info to ingester SKIP_DIRS

--- backend/load_graph.py
+++ backend/load_graph.py
@@ -8,7 +8,7 @@ from neo4j import GraphDatabase
 
 NEO4J_URI      = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER     = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "surgeon1234")
 
 GDS_GRAPH_NAME = "call_graph"
 
